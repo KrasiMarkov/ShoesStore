4f43c5d19372380954a6f64e68c024468bf7b2b7
KrasiMarkov <64689391+KrasiMarkov@users.noreply.github.com>
2021-07-13 19:28:45 +0300

    Created project

---
+++ ShoesStore/ShoesStore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ShoesStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
