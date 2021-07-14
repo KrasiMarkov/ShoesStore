49a2b0901e511b2f5e237a31744b97d4579cd9ae
KrasiMarkov <64689391+KrasiMarkov@users.noreply.github.com>
2021-07-14 10:41:33 +0300

    Created categories

--- ShoesStore/ShoesStore/Data/Infrastructure/ApplicationBuilderExtensions.cs
+++ ShoesStore/ShoesStore/Data/Infrastructure/ApplicationBuilderExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using ShoesStore.Data.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,7 +20,28 @@ namespace ShoesStore.Data.Infrastructure
 
             data.Database.Migrate();
 
+            SeedCategories(data);
+
             return app;
+        }
+
+        private static void SeedCategories(ShoesStoreDbContext data)
+        {
+            if (data.Categories.Any())
+            {
+                return;
+
+            }
+
+            data.Categories.AddRange(new[]
+            {
+                new Category{Name = "Mens"},
+                new Category{Name = "Ladies"},
+                new Category{Name = "Kids" }
+
+            });
+
+            data.SaveChanges();
         }
     }
 }
